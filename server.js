67afa1ed47e3fa09717b93aa4176c60cc6a3d297
JorGBit <113969001+JorGBit@users.noreply.github.com>
2023-01-03 15:56:06 +0100

    subiendo archivo

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const newUser = require('./controllers/usuarios/newUser');
+const morgan = require('morgan'); //esto envía más información sobre las consultas realizadas en postman
+
+require('dotenv').config();
+///Conexión a la base de datos
+const app = express();
+
+//Deserializar body formato raw
+app.use(express.json()); //super necesario para poder leer los json del body de postman raw. "deserializar"
+app.use(morgan('dev'));
+//Endpoints. COMENZAMOS AQUÍ A INCLUIR LOS ENDPOINTS DEL PROYECTO
+
+app.post('/register', newUser);
+
+//gestión de errores: Error y Not Found (Middleware de errory not found)
+app.use((error, req, res, _) => {
+    console.error(error);
+    res.status(error.httpStatus || 500);
+    res.send({
+        status: 'Error',
+        message: error.message,
+    });
+});
+
+app.use((req, res) => {
+    res.status(404);
+    res.send({
+        status: 'Error',
+        message: 'Not found',
+    });
+});
+
+//Ponemos a la escucha el servidor
+app.listen(process.env.PORT, () => {
+    console.log(`Server listening at http://localhost:${process.env.PORT}`);
+});
