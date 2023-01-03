153db19c9bd2b18248bb90187e41b5eb40a10615
JorGBit <113969001+JorGBit@users.noreply.github.com>
2023-01-03 15:49:02 +0100

    Update initDB.js

--- db/initDB.js
+++ db/initDB.js
@@ -8,30 +8,30 @@ async function main() {
 
         console.log('Eliminando tablas en caso de que existan');
 
-        await connection.query(`DROP TABLE IF EXISTS foto_notas`);
-        await connection.query(`DROP TABLE IF EXISTS notas`);
-        await connection.query(`DROP TABLE IF EXISTS usuario`);
+        await connection.query(`DROP TABLE IF EXISTS photo_notes`);
+        await connection.query(`DROP TABLE IF EXISTS notes`);
+        await connection.query(`DROP TABLE IF EXISTS user`);
 
         console.log('Tablas eliminadas');
 
         console.log('Creando tablas...');
 
         await connection.query(
-            `CREATE TABLE IF NOT EXISTS usuario (
+            `CREATE TABLE IF NOT EXISTS user (
                 id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-                nombre_usuario VARCHAR(100) UNIQUE NOT NULL,
-                nombre VARCHAR (50) NOT NULL,
-                apellidos VARCHAR(100) NOT NULL,
+                username VARCHAR(100) UNIQUE NOT NULL,
+                name VARCHAR (50),
+                lastname VARCHAR(100),
                 email VARCHAR(255) UNIQUE NOT NULL,
-                contrasena VARCHAR(255) NOT NULL,
+                password VARCHAR(255) NOT NULL,
                 avatar VARCHAR(255),
-                fechacumple DATE
+                birthday DATE
                 
             )`
         );
 
         await connection.query(
-            `CREATE TABLE IF NOT EXISTS notas (
+            `CREATE TABLE IF NOT EXISTS notes (
                 id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
                 idNota INT UNSIGNED NOT NULL,
                 titulo_nota VARCHAR(200) NOT NULL,
@@ -41,7 +41,7 @@ async function main() {
         );
 
         await connection.query(
-            `CREATE TABLE IF NOT EXISTS foto_notas (
+            `CREATE TABLE IF NOT EXISTS photo_notes (
                 id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
                 nombre VARCHAR(255) NOT NULL,
                 idNota INT UNSIGNED NOT NULL,
@@ -50,22 +50,17 @@ async function main() {
         );
         console.log('Tablas creadas');
 
-        //INSERTAMOS LOS DATOS CUANDO TENGAMOS CLARO EL FUNCIONAMIENTO DE LA BASE DE DATOS
+        // INSERTAMOS LOS DATOS CUANDO TENGAMOS CLARO EL FUNCIONAMIENTO DE LA BASE DE DATOS
 
         // console.log('Insertamos unos datos de prueba');
 
         // await connection.query(
-        //     `INSERT INTO usuario (nombre_usuario, email, contraseña)
+        //     `INSERT INTO user (username, email, password)
         //     VALUES ('userPrueba', 'prueba@gmail.com', '123456')`
         // );
 
         // await connection.query(
-        //     `INSERT INTO notas (nombre_usuario, email, contraseña)
-        //     VALUES ('userPrueba', 'prueba@gmail.com', '123456')`
-        // );
-
-        // await connection.query(
-        //     `INSERT INTO foto_notas (nombre_usuario, email, contraseña)
+        //     `INSERT INTO notes (username, email, password)
         //     VALUES ('userPrueba', 'prueba@gmail.com', '123456')`
         // );
 
