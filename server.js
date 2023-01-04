e4fad4c3530925634d182186179d90f584fbb2e1
JorGBit <113969001+JorGBit@users.noreply.github.com>
2023-01-04 11:45:01 +0100

    actualizado

--- server.js
+++ server.js
@@ -10,10 +10,13 @@ const app = express();
 //Deserializar body formato raw
 app.use(express.json()); //super necesario para poder leer los json del body de postman raw. "deserializar"
 app.use(morgan('dev'));
+//MIDDLEWARES*******************************
+const isAuth = require('./middlewares/isAuth');
 
 //CONTROLADORES DE USUARIOS**************************************************
 const newUser = require('./controllers/usuarios/newUser');
 const loginUser = require('./controllers/usuarios/loginUser');
+const getUser = require('./controllers/usuarios/getUser');
 
 //Endpoints. COMENZAMOS AQUÍ A INCLUIR LOS ENDPOINTS DEL PROYECTO
 //REGISTRO DE USUARIO
@@ -21,6 +24,12 @@ app.post('/register', newUser);
 
 //LOGIN DE USUARIO
 app.post('/login', loginUser);
+
+//DEVOLVER INFO DE UN USUARIO
+app.get('/users/:idUser', getUser);
+
+//MODIFICAR EMAIL O USUARIO
+app.put('/users/:idUser', isAuth);
 //gestión de errores: Error y Not Found (Middleware de errory not found)
 
 //######################################-NOTAS-
