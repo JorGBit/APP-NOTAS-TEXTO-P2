0f202773b189bbae31df836a77853b2e5eb64ba9
JorGBit <113969001+JorGBit@users.noreply.github.com>
2023-01-10 16:27:27 +0100

    actualizado

--- controllers/notas/newNotes.js
+++ controllers/notas/newNotes.js
@@ -1,13 +1,12 @@
-// Insertar nueva nota
-
 const getDB = require('../../db/getDB');
 const { generateError, validateSchema } = require('../../helpers');
 const newNoteSchema = require('../../schemas/newNoteSchema');
+
 const newNotes = async (req, res, next) => {
     let connection;
     try {
         connection = await getDB();
-        // Recuperamos el id del usuario loguiado
+
         const idUserAuth = req.userAuth.id;
 
         await validateSchema(newNoteSchema, req.body);
@@ -16,23 +15,16 @@ const newNotes = async (req, res, next) => {
         // Recuperamos los datos de la nota
         await connection.query(
             `
-            INSERT INTO notes (tittle, category, text, id)
+            INSERT INTO notes (tittle, category, text, idUser)
             VALUES (?, ?, ?, ?)
         `,
             [tittle, category, text, idUserAuth]
         );
-        // const { titulo_nota, categoria, texto_nota } = req.body;
-        // if (!titulo_nota || !categoria || !texto_nota) {
-        //     throw generateError(
-        //         '¡El título de la nota, la categoría y el texto de la nota son datos necesarios para crear la nota!',
-        //         400
-        //     );
-        // }
+
         res.send({
             status: 'ok',
             message: '¡La nota ha sido creada correctamente!',
-            data: { tittle, category, text },
-            // datos: { titulo_nota, categoria, texto_nota },
+            data: { idUserAuth, category, tittle, text },
         });
     } catch (error) {
         next(error);
